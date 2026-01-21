1206e24d8b01230424447eef7224b316a25c095d
Abi <debrah.ab@northeastern.edu>
2026-01-21 13:03:53 +0000

    adds failing tests

--- test_pig_latin.py
+++ test_pig_latin.py
@@ -1,4 +1,11 @@
 from main import pig_latin
 
-def test_pig_latin():
-    assert 2+2 == 4
+def test_pig_latin_happy():
+    assert pig_latin("Hello World") == "ellohay orldway"
+    assert pig_latin("phone") == "onephay"
+    assert pig_latin("apple") == "appleyay"
+
+def test_pig_latin_unhappy():
+    assert pig_latin("abigail") != "bigailayay"
+    assert pig_latin("programming") != "rogrammingpay"
+    assert pig_latin("nice day") != "icenayayday"
