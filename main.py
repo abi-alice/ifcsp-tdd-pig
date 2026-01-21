baca7de986c178a06dad5f9e298bb29a6b0679ab
Abi <debrah.ab@northeastern.edu>
2026-01-21 13:09:44 +0000

    adds main code

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-def pig_latin():
-    return True
+def pig_latin(text):
+    vowels = "aeiou"
+    lower_text = text.lower()
+    words = lower_text.split()
+    result = []
+
+    for word in words:
+        if word[0] in vowels:
+            pig_word = word + "yay"
+        else:
+            first_vowel = 0
+            for i, char in enumerate(word):
+                if char in vowels:
+                    first_vowel = i
+                    break
+            pig_word = word[first_vowel:] + word[:first_vowel] + "ay"
+        
+        result.append(pig_word)
+    return " ".join(result)
